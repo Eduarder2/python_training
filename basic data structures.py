f6204a1c2b2bc716897551a417a2f4e220c8e120
Eduarder2 <95880422+Eduarder2@users.noreply.github.com>
2024-10-27 23:43:42 +0300

    Add files via upload

---
+++ basic data structures.py
@@ -0,0 +1,18 @@
+# "1st program"
+print(9 ** 0.5 * 5, end='\n\n')
+
+# "2nd program"
+print(9.99 > 9.98 and 1000 != 1000.1, end='\n\n')
+
+# "3rd program"
+a = 2 * 2 + 2
+print(a)
+b = 2 * (2 + 2)
+print(b)
+print(a == b, end='\n\n')
+
+# "4th program"
+NumStr = '123.456'
+NumFloat = float(NumStr)
+print(int(NumFloat * 10) % 10)
+
