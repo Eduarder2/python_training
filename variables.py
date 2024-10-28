185a989f86720a3d46d1db2daf54bbb1d9480bef
Eduarder2 <95880422+Eduarder2@users.noreply.github.com>
2024-10-28 12:59:51 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,7 @@
+homeworks_done = 12
+hours_spent = 1.5
+course = 'Python'
+speed = hours_spent / homeworks_done
+print('Курс: ', course, ', всего задач: ', homeworks_done,
+      ', затрачено часов: ', hours_spent, ', среднее время выполнения: ',
+      speed, ' часа.', sep='')
