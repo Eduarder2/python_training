d200098f3286a63ab41e2dfdd6c23f82fe65c968
Eduarder2 <95880422+Eduarder2@users.noreply.github.com>
2024-10-28 12:18:00 +0300

    Add files via upload

---
+++ dynamic typing.py
@@ -0,0 +1,8 @@
+name = 'Eduard'
+print('My name: ', name)
+age = 24
+print('My age: ', age)
+age += 10
+print('My age (ten years later): ', age)
+is_student = True
+print('Am I student? ', is_student)
